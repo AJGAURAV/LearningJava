8b9064b522a025be30433c2e234a641ec2b7ea2c
Gaurav Tomar <73416549+AJGAURAV@users.noreply.github.com>
2022-04-04 21:44:41 +0530

    Update p1_task01_firstJavaProgram.java

--- JavaPractice/src/Unit_01/p1_task01_firstJavaProgram.java
+++ JavaPractice/src/Unit_01/p1_task01_firstJavaProgram.java
@@ -4,6 +4,7 @@ public class p1_task01_firstJavaProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Namaste Visw :)\n");
+		
+		System.out.println("Aham Ajay Gaurav");
 	}
-
 }
